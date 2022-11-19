bdea84a35b66b479688cd337292e909221c37a86
awesomeSwam <108855819+awesomeSwam@users.noreply.github.com>
2022-11-19 20:36:40 +0900

    request limit

--- server.js
+++ server.js
@@ -18,31 +18,31 @@ app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // limiter
-const rateLimit = require("express-rate-limit");
-const {
-  FIRST_LIMITER_WINDOWMS, FIRST_LIMITER_MAX, POP_LIMITER_WINDOWMS, POP_LIMITER_MAX,
-  LEADERBOARD_LIMITER_WINDOWMS, LEADERBOARD_LIMITER_MAX, SCHOOL_LIMITER_WINDOWMS, SCHOOL_LIMITER_MAX
-} = process.env;
+// const rateLimit = require("express-rate-limit");
+// const {
+//   FIRST_LIMITER_WINDOWMS, FIRST_LIMITER_MAX, POP_LIMITER_WINDOWMS, POP_LIMITER_MAX,
+//   LEADERBOARD_LIMITER_WINDOWMS, LEADERBOARD_LIMITER_MAX, SCHOOL_LIMITER_WINDOWMS, SCHOOL_LIMITER_MAX
+// } = process.env;
 
-const firstLimiter = rateLimit({
-  windowMs: FIRST_LIMITER_WINDOWMS,
-  max: FIRST_LIMITER_MAX
-});
+// const firstLimiter = rateLimit({
+//   windowMs: FIRST_LIMITER_WINDOWMS,
+//   max: FIRST_LIMITER_MAX
+// });
 
-const popLimiter = rateLimit({
-  windowMs: POP_LIMITER_WINDOWMS,
-  max: POP_LIMITER_MAX
-});
+// const popLimiter = rateLimit({
+//   windowMs: POP_LIMITER_WINDOWMS,
+//   max: POP_LIMITER_MAX
+// });
 
-const leaderboardLimiter = rateLimit({
-  windowMs: LEADERBOARD_LIMITER_WINDOWMS,
-  max: LEADERBOARD_LIMITER_MAX
-});
+// const leaderboardLimiter = rateLimit({
+//   windowMs: LEADERBOARD_LIMITER_WINDOWMS,
+//   max: LEADERBOARD_LIMITER_MAX
+// });
 
-const schoolLimiter = rateLimit({
-  windowMs: SCHOOL_LIMITER_WINDOWMS,
-  max: SCHOOL_LIMITER_MAX
-})
+// const schoolLimiter = rateLimit({
+//   windowMs: SCHOOL_LIMITER_WINDOWMS,
+//   max: SCHOOL_LIMITER_MAX
+// })
 
 // Routers
 const firstRouter = require("./router/first");
